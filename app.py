f7ee50caa0c1027a3240b15d731054af92cb0a3f
Dash <dascastrillo@gmail.com>
2020-05-26 20:22:03 +0200

    cambios app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 #Para hacer una API hay que instalar en el visual studio code el flask
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+import sys
+import logging
 app = Flask (__name__)
+
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 #importar los datos libros
 from libros import libros
